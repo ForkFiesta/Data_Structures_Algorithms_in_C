0700e85c7c31839cc307e7349dd9403ebdc473ba
Raul Resendez <raul.resendez@proton.me>
2024-11-03 16:41:18 -0600

    Improve test for edge cases

--- Linked-List-w-Tail/linkedlist.c
+++ Linked-List-w-Tail/linkedlist.c
@@ -88,7 +88,7 @@ void pushBack(list *lst, int value){
 
 int popback(list *lst){
     if(lst->head->next == lst->tail){
-        printf("Cannot pop from empty list.\n");
+        // fprintf(stderr,"Cannot pop from empty list.\n");
         return -1;
     }
 
@@ -107,6 +107,107 @@ int popback(list *lst){
 
 
 
+bool find(list *lst, int key){
+    if(lst->head->next == lst->tail){
+        // printf("Cannot search an empty list.\n");
+        return false;
+    }
+    
+
+    node *p = lst->head;
+
+    while(p != lst->tail){
+        if (p->data == key){
+            return true;
+        }
+        p = p->next;
+    }
+    return false;
+
+}
+
+void erase(list *lst, int key){
+    if(is_empty(lst)){
+        // fprintf(stderr, "Cannot erase from empty list.\n");
+        return;
+    }
+
+    node *p = lst->head;
+    while(p != lst->tail){
+        if(p->next->data == key){
+            node *t = p->next;
+            p->next = t->next;
+            p->next->prev = p;
+            free(t);
+        }
+        else{
+            p = p->next;
+        }
+    }
+
+}
+
+bool is_empty(list *lst){
+    return (lst->head->next == lst->tail);
+}
+
+
+node *getNthNode(list *lst, int n){
+    if(is_empty(lst)){
+        // fprintf(stderr, "Cannot search an empty list\n");
+        return NULL;
+    }
+
+    int count = 0;
+    node *p = lst->head->next;
+    while(count != n){
+        p = p->next;
+        count++;
+    }
+
+    return p;
+}
+
+
+
+void addAfter(node *nd, int key){
+    if(nd == NULL){
+        fprintf(stderr, "Inputted node is empty. Exiting program.\n");
+        exit(1);
+    }
+    node *new_node = malloc(sizeof(node));
+    if(new_node == NULL){
+        fprintf(stderr, "Error allocating memory for new node. Terminating program.\n");
+        exit(1);
+    }
+
+    new_node->data = key;
+    new_node->next = nd->next;
+    new_node->prev = nd;
+    nd->next->prev = new_node;
+    nd->next = new_node;
+}
+
+void addBefore(node *nd, int key){
+    if(nd == NULL){
+        fprintf(stderr, "Inputted node is empty. Exiting program.\n");
+        exit(1);
+    }
+    node *new_node = malloc(sizeof(node));
+    if(new_node == NULL){
+        fprintf(stderr, "Error allocating memory for new node. Terminating program.\n");
+        exit(1);
+    }
+
+    new_node->data = key;
+    new_node->next = nd;
+    new_node->prev = nd->prev;
+    nd->prev->next = new_node;
+    nd->prev = new_node;
+}
+
+
+
 
 /* Tests */
 void run_all_tests(){
@@ -114,28 +215,39 @@ void run_all_tests(){
     test_initialize_list();
     test_destroy_list();
     test_pushFront();
+    test_is_empty();
     test_pushBack();
     test_popBack();
+    test_find();
+    test_erase();
+    test_getNthNode();
+    test_addAfter();
+    test_addBefore();
     printf("\033[1;32mPassed All Tests\033[0m\n");
 }
 
 void test_initialize_list(){
     printf("\033[0;33mTesting Initialize List ..... \033[0m");
-    list *test = initialize_list();
-    assert(test != NULL && test->head!= NULL && test->tail != NULL && test->size == 0);
-    free(test->head);
-    free(test->tail);
-    free(test);
+    list *lst = initialize_list();
+    assert(lst != NULL);
+    assert(lst->head != NULL);
+    assert(lst->tail != NULL);
+    assert(lst->head->next == lst->tail);
+    assert(lst->tail->prev == lst->head);
+    destroy_list(lst);
+    free(lst);
     printf("\033[0;32mPassed.\033[0m\n");
 }
 
 void test_destroy_list(){
     printf("\033[0;33mTesting Destroy List ........ \033[0m");
-    list *test = initialize_list();
-    assert(test != NULL && test->head!= NULL && test->tail != NULL && test->size == 0);
-    destroy_list(test);
-    assert(test->head == NULL && test->tail == NULL && test->size == 0);
-    free(test);
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    pushBack(lst, 2);
+    destroy_list(lst);
+    assert(lst->head == NULL);
+    assert(lst->tail == NULL);
+    free(lst);
 
     printf("\033[0;32mPassed.\033[0m\n");
 
@@ -143,14 +255,13 @@ void test_destroy_list(){
 
 void test_pushFront(){
     printf("\033[0;33mTesting Push Front .......... \033[0m");
-    list *test = initialize_list();
-    pushFront(test, 4);
-    pushFront(test, 6);
-    pushFront(test, 7);
-    pushFront(test, 3);
-    assert(test->head->next->data == 3 && test->tail->prev->data == 4);
-    destroy_list(test);
-    free(test);
+    list *lst = initialize_list();
+    pushFront(lst, 1);
+    assert(lst->head->next->data == 1);
+    pushFront(lst, 2);
+    assert(lst->head->next->data == 2);
+    destroy_list(lst);
+    free(lst);
     printf("\033[0;32mPassed.\033[0m\n");
 
 
@@ -158,37 +269,122 @@ void test_pushFront(){
 
 void test_pushBack(){
     printf("\033[0;33mTesting PushBack ............ \033[0m");
-    list *test = initialize_list();
-    pushBack(test, 8);
-    pushBack(test, 4);
-    pushBack(test, 62);
-    pushBack(test, 83);
-    pushBack(test, 24);
-    pushBack(test, 82);
-    pushBack(test, 9);
-    pushBack(test, 4);
-    assert(test->head->next->data == 8 && test->tail->prev->data == 4);
-    destroy_list(test);
-    free(test);
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    assert(lst->tail->prev->data == 1);
+    pushBack(lst, 2);
+    assert(lst->tail->prev->data == 2);
+    destroy_list(lst);
+    free(lst);
     printf("\033[0;32mPassed.\033[0m\n");
 
 }
 
 void test_popBack(){
     printf("\033[0;33mTesting popBack ............. \033[0m");
-    list *test = initialize_list();
-    pushBack(test, 8);
-    pushBack(test, 4);
-    pushBack(test, 62);
-    pushBack(test, 83);
-    pushBack(test, 24);
-    pushBack(test, 82);
-    pushBack(test, 9);
-    pushBack(test, 4);
-    assert(popback(test) == 4 && popback(test) == 9 && popback(test) == 82);
-    destroy_list(test);
-    free(test);
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    pushBack(lst, 2);
+    assert(popback(lst) == 2);
+    assert(popback(lst) == 1);
+    assert(popback(lst) == -1); // Edge case: popping from empty list
+    destroy_list(lst);
+    free(lst);
     printf("\033[0;32mPassed.\033[0m\n");
 
     
+}
+
+
+void test_find(){
+    printf("\033[0;33mTesting find ................ \033[0m");
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    pushBack(lst, 2);
+    assert(find(lst, 1) == true);
+    assert(find(lst, 3) == false);
+    destroy_list(lst);
+    free(lst);
+    printf("\033[0;32mPassed.\033[0m\n");
+
+
+
+}
+
+
+void test_erase(){
+    printf("\033[0;33mTesting Erase ............... \033[0m");
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    pushBack(lst, 2);
+    pushBack(lst, 3);
+    erase(lst, 2);
+    assert(find(lst, 2) == false);
+    erase(lst, 1);
+    assert(find(lst, 1) == false);
+    erase(lst, 3);
+    assert(find(lst, 3) == false);
+    destroy_list(lst);
+    free(lst);
+    printf("\033[0;32mPassed.\033[0m\n");
+
+
+}
+
+
+void test_getNthNode(){
+    printf("\033[0;33mTesting Get Nth Node ........ \033[0m");
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    pushBack(lst, 2);
+    pushBack(lst, 3);
+    node *n = getNthNode(lst, 1);
+    assert(n->data == 2);
+    n = getNthNode(lst, 0);
+    assert(n->data == 1);
+    destroy_list(lst);
+    free(lst);
+    printf("\033[0;32mPassed.\033[0m\n");
+
+}
+
+void test_addAfter(){
+    printf("\033[0;33mTesting AddAfter ............ \033[0m");
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    pushBack(lst, 2);
+    node *n = getNthNode(lst, 0);
+    addAfter(n, 3);
+    assert(n->next->data == 3);
+    destroy_list(lst);
+    free(lst);
+    printf("\033[0;32mPassed.\033[0m\n");
+
+}
+
+void test_addBefore(){
+    printf("\033[0;33mTesting AddBefore ........... \033[0m");
+    list *lst = initialize_list();
+    pushBack(lst, 1);
+    pushBack(lst, 2);
+    node *n = getNthNode(lst, 1);
+    addBefore(n, 3);
+    assert(n->prev->data == 3);
+    destroy_list(lst);
+    free(lst);
+    printf("\033[0;32mPassed.\033[0m\n");    
+}
+
+
+void test_is_empty(){
+ printf("\033[0;33mTesting is_Empty ............ \033[0m");
+    list *lst = initialize_list();
+    assert(is_empty(lst) == true);
+    pushBack(lst, 1);
+    assert(is_empty(lst) == false);
+    destroy_list(lst);
+    free(lst);
+    printf("\033[0;32mPassed.\033[0m\n");    
+
+
 }
--- Linked-List-w-Tail/linkedlist.h
+++ Linked-List-w-Tail/linkedlist.h
@@ -24,6 +24,12 @@ void destroy_list(list *lst);
 void pushFront(list *lst, int value);
 void pushBack(list *lst, int value);
 int popback(list *lst);
+bool find(list *lst, int key);
+void erase(list *lst, int key);
+bool is_empty(list *lst);
+node *getNthNode(list *lst, int n);
+void addAfter(node *nd, int key);
+void addBefore(node *nd, int key);
 
 
 
@@ -40,6 +46,20 @@ void test_destroy_list();
 
 void test_pushFront();
 
+void test_is_empty();
+
 void test_pushBack();
 
 void test_popBack();
+
+void test_find();
+
+void test_erase();
+
+void test_getNthNode();
+
+void test_addAfter();
+
+void test_addBefore();
+
+
