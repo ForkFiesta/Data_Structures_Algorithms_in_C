42f584ab00783b11b171b8249cc21f67307c7303
Raul Resendez <raul.resendez@proton.me>
2024-11-02 19:54:31 -0500

    Complete test function definitions

--- Linked_list/linked_list.c
+++ Linked_list/linked_list.c
@@ -4,16 +4,58 @@
 #include <stdbool.h>
 #include <assert.h>
 
+list *initialize_list(){
+    list *lst = malloc(sizeof(list));
+    if(lst==NULL){
+        printf("Error allocating memory. Ending program\n");
+        exit(EXIT_FAILURE);
+    }
 
+    lst->size = 0;
+    lst->head = NULL;
+}
+
+
+
+
+
+
+
+
+
+/* Tests */
+
+void run_all_tests(){
+    printf("\033[1;33m Running all tests\033[0m\n");
+    test_initalize_list();
+    test_push();
+    test_destroy_list();
+    test_pop();
+    test_search();
+    test_remove_values();
+    test_remove_value();
+    test_remove_values();
+    test_remove_from_end();
+    test_insert_at_end();
+    test_find_smallest();
+    test_find_largest();
+    test_get_value_from_index();
+    test_remove_from_index();
+    printf("\033[1;32mPassed All Tests Succesfully\033[0m\n");
+
+}
 
 void test_initalize_list(){
+    printf("\033[0;33mTesting initalize list...\n");
     list *test = initialize_list();
     assert(test != NULL && test->head == NULL && test->size==0);
     destroy_list(test);
 
+
 }
 
 void test_push(){
+    printf("Testing push ...\n");
     list *test = initialize_list();
     push(test, 3);
     push(test, 5);
@@ -24,9 +66,26 @@ void test_push(){
     push(test, 55);
     assert(test->size==7 && test->head->data==55 && test->head->next->data == 12);
     destroy_list(test);
+
+}
+
+
+void test_destroy_list(){
+    printf("Testing Destroy List...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 55);
+    destroy_list(test);
+    assert(test->head == NULL);
 }
 
 void test_pop(){
+    printf("Testing pop()...\n");
     list *test = initialize_list();
     push(test, 3);
     push(test, 5);
@@ -41,5 +100,148 @@ void test_pop(){
 }
 
 void test_search(){
-    
+    printf("Testing search ... \n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 55);
+    assert(search(test, 2) == 1);
+    destroy_list(test);
+
 }
+
+void test_remove_values(){
+    printf("Testing Remove_Values...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 3);
+    assert(search(test, 3)==1);
+    remove_values(test, 3);
+    assert(search(test, 3)== -1);
+    destroy_list(test);
+}
+
+
+void test_remove_value(){
+    printf("Testing Remove First Value Found...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 3);
+    remove_value(test, 3);
+    assert(test->head->data==12 && search(test, 3) == 1);
+    destroy_list(test);
+
+}
+
+
+void test_remove_from_end(){
+    printf("Testing Removing from End...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+
+    int tv = remove_from_end(test);
+    assert(tv == 3);
+
+}
+
+void test_insert_at_end(){
+    printf("Testing Insert at end...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 3);
+    insert_at_end(test, 96);
+    remove_value(test, 3);
+    assert(test->head->data==12 && search(test, 3) == 1);
+    destroy_list(test);
+}
+
+
+void test_find_smallest(){
+     printf("Testing Find Smallest...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 3);
+    assert(find_smallest(test)==2);
+    destroy_list(test);
+}
+
+
+void test_find_largest(){
+     printf("Testing Find Largest...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 3);
+    assert(find_smallest(test)==12);
+    destroy_list(test);
+
+}
+
+
+void test_remove_from_index(){
+    printf("Testing Remove from Index...\033[0m\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 3);
+    assert(remove_from_index(test, 1) == 12);
+    destroy_list(test);
+
+}
+
+void test_get_value_from_index(){
+    printf("Testing Get Value from Index...\n");
+    list *test = initialize_list();
+    push(test, 3);
+    push(test, 5);
+    push(test, 9);
+    push(test, 2);
+    push(test, 7);
+    push(test, 12);
+    push(test, 3);
+    assert(get_value_from_index(test, 3) == 2);
+    destroy_list(test);
+
+}
+
+
+
+
+
--- Linked_list/linked_list.h
+++ Linked_list/linked_list.h
@@ -19,13 +19,13 @@ list *initialize_list(); // initializes list object with pointer to head
 void push(list *lst, int value); // pushes element at beginning of list
 int pop(list *lst); // pops and returns element at head or top of list
 int search(list *lst, int value); // Returns 1 if value found, -1 if not found
-void remove_value(list *lst); // removes first found value 
-void remove_values(list *lst); // removes all found values
+void remove_value(list *lst, int value); // removes first found value 
+void remove_values(list *lst, int values); // removes all found values
 void insert_at_end(list *lst, int value); // inserts element at end of list
 int remove_from_end(list *lst); // removes element from end of list and returns it
 void destroy_list(list *lst); // frees data for all elements in list
-void find_largest(list *lst); // finds largest value in list
-void find_smallest(list *lst); // find smallest value in list
+int find_largest(list *lst); // finds largest value in list
+int find_smallest(list *lst); // find smallest value in list
 void remove_from_index(list *lst, int index); // indices list and removes item at index n
 
 
